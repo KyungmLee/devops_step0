0e669d9c811fb2d6c1109d166a4549e5f78b48e2
kyungmi <imlkm70@daum.net>
2024-12-22 10:25:41 +0900

    test내용 실패

--- src/test/java/kr/ac/koreatech/devops_step0/DevopsStep0ApplicationTests.java
+++ src/test/java/kr/ac/koreatech/devops_step0/DevopsStep0ApplicationTests.java
@@ -1,5 +1,8 @@
 package kr.ac.koreatech.devops_step0;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +13,19 @@ class DevopsStep0ApplicationTests {
 	void contextLoads() {
 	}
 
+	int add(int a, int b) {
+		return a + b;
+	}
+
+	@Test
+	void add_test1() {
+		int result = add(10, 20);
+		assertEquals(30, result);
+	}
+
+	@Test
+	void add_test2() {
+		int result = add(10, 20);
+		assertEquals(10, result);
+	}
 }
