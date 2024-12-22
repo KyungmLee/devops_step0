684bbb4d296e3c4b930f35639babd272e2b635fb
kyungmi <imlkm70@daum.net>
2024-12-22 11:36:39 +0900

    Docker파일 생성 및 test 수정정

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM openjdk:17-jdk-slim
+
+# 작업폴더 생성
+WORKDIR /app
+
+# 실행한 jar 파일을 app.jar 파일로 복사
+COPY build/libs/*SNAPSHOT.jar app.jar
+
+# 외부 노출 포트 설정
+EXPOSE port
+
+# 실행 명령어를 파라미터 형식으로 전달
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/test/java/kr/ac/koreatech/devops_step0/DevopsStep0ApplicationTests.java
+++ src/test/java/kr/ac/koreatech/devops_step0/DevopsStep0ApplicationTests.java
@@ -23,9 +23,9 @@ class DevopsStep0ApplicationTests {
 		assertEquals(30, result);
 	}
 
-	@Test
-	void add_test2() {
-		int result = add(10, 20);
-		assertEquals(10, result);
-	}
+	// @Test
+	// void add_test2() {
+	// 	int result = add(10, 20);
+	// 	assertEquals(10, result);
+	// }
 }
